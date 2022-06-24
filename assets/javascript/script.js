335a9353336c6d27023cd40fe31a3083b663da22
Your Name <koober@Brians-MacBook-Pro.local>
2022-06-23 22:30:41 -0400

    Fixed undefined code and added comments

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Arrays that contain the different possible character choices
 var characterLength = 8;
 var choiceArr = [];
 
@@ -9,26 +10,26 @@ var upperCaseArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',
 var numberArr = ['1','2','3','4','5','6','7','8','9','0',];
 var specialCharArr = ['!','@','#','$','%','^','&','*','(',')'];
 
-
+// Function to generate Password using the chosen prompts
 function generatePassword() { 
   var password = "";
   for(var i = 0; i < characterLength; i++) {
-    var randomIndex = Math.floor(Math.random() + choiceArr.length)
+    var randomIndex = Math.floor(Math.random() * choiceArr.length)
     password = password + choiceArr[randomIndex];
   }
   return password;
 }
 
+// Prompts/Criteria that are asked in order to generate the password
 function getPrompts() {
   choiceArr = [];
   
-  characterLength = parseInt(prompt("How many characters would you like for your password to contain? (Choose a number between 8 and 128)"));
+  characterLength = prompt("How many characters would you like for your password to contain? (Choose a number between 8 and 128)");
   
   if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
     alert("Please select a number that is between 8 and 128.");
     return false;
   }
-  
   if (confirm("Would you like to use lowercase letters in your password?")) {
     choiceArr = choiceArr.concat(lowerCaseArr);
   }
@@ -42,7 +43,6 @@ function getPrompts() {
     choiceArr = choiceArr.concat(specialCharArr);
   }
   return true;
-  
 }
 
 // Write password to the #password input
@@ -55,10 +55,8 @@ function writePassword() {
     passwordText.value = newPassword;
   } else {
     passwordText.value = "";
-
   }
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
