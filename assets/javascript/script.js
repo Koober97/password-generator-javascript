5f50d6ea41c6b620e93dff37b6e5bde82b57435f
Your Name <koober@Brians-MacBook-Pro.local>
2022-06-23 22:02:22 -0400

    most code done, fix undefined error

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,24 +1,62 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-  var length = prompt("How long?")
-  console.log(length);
-  if (length < 8 || length > 128) {
-    alert ("Password must be between 8 and 128 characters");
-    return generatePassword();
+var characterLength = 8;
+var choiceArr = [];
+
+var lowerCaseArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCaseArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numberArr = ['1','2','3','4','5','6','7','8','9','0',];
+var specialCharArr = ['!','@','#','$','%','^','&','*','(',')'];
+
+
+function generatePassword() { 
+  var password = "";
+  for(var i = 0; i < characterLength; i++) {
+    var randomIndex = Math.floor(Math.random() + choiceArr.length)
+    password = password + choiceArr[randomIndex];
   }
-  c
-  return "password";
+  return password;
+}
+
+function getPrompts() {
+  choiceArr = [];
+  
+  characterLength = parseInt(prompt("How many characters would you like for your password to contain? (Choose a number between 8 and 128)"));
+  
+  if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    alert("Please select a number that is between 8 and 128.");
+    return false;
+  }
+  
+  if (confirm("Would you like to use lowercase letters in your password?")) {
+    choiceArr = choiceArr.concat(lowerCaseArr);
+  }
+  if (confirm("Would you like to use uppercase letters in your password?")) {
+    choiceArr = choiceArr.concat(upperCaseArr);
+  }
+  if (confirm("Would you like to use numbers in your password?")) {
+    choiceArr = choiceArr.concat(numberArr);
+  }
+  if (confirm("Would you like to use special characters in your password?")) {
+    choiceArr = choiceArr.concat(specialCharArr);
+  }
+  return true;
+  
 }
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var correctPrompts = getPrompts();
 
-  passwordText.value = password;
+  if (correctPrompts) {
+    var newPassword = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = newPassword;
+  } else {
+    passwordText.value = "";
 
+  }
 }
 
 // Add event listener to generate button
